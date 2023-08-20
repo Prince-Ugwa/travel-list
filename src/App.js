c64685c37d6ffed05b8f324d296dc002a4aab63d
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2023-08-20 23:25:35 +0100

    implementing controlled element allowing react to keep data in syn with the state od the dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
   { id: 3, description: "Jackets", quantity: 24, packed: false },
-  { id: 4, description: "Hats", quantity: 12, packed: true },
+  { id: 4, description: "Hats", quantity: 12, packed: false },
 ];
 
 export default function App() {
@@ -26,20 +28,34 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
   }
   return (
-    <form className="add-form" onClick={handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you want for your 🥰 trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}{" "}
       </select>
-      <input type="text" placeholder="item...." />
+      <input
+        type="text"
+        placeholder="item...."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
