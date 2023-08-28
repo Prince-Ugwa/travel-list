d6f7146fda4cc3c0d2c4daca69aaa5c1344513c2
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2023-08-28 04:45:09 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -8,11 +8,16 @@ const initialItems = [
 ];
 
 export default function App() {
+  //state add new item to the array, we need the sate update also in the packing list so we need to state up to the closest common comoponent.
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItem={handleAddItems} />
+      <PackingList items={items} />
       <Stat />
     </div>
   );
@@ -27,9 +32,13 @@ function Logo() {
   );
 }
 
-function Form() {
+function Form({ onAddItem }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
+  // //this state add new item to the array, we need the sate update also in the packing list so we need to state up to the closest common comoponent.
+  // const [items, setItems] = useState([]);
+
   function handleSubmit(e) {
     e.preventDefault();
 
@@ -37,6 +46,8 @@ function Form() {
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+
+    onAddItem(newItem);
     setDescription("");
     setQuantity(1);
   }
@@ -67,12 +78,25 @@ function Form() {
   );
 }
 
-function PackingList() {
+// function PackingList() {
+//   return (
+//     <div className="list">
+//       <ul>
+//         {initialItems.map((item) => (
+//           <Item item={item} key={item.id} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+//updated
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
+        {items.map((item) => (
+          <Item item={item} key={item.ids} />
         ))}
       </ul>
     </div>
